a5c096b445deff9a70bf02033b6b3c409d547a78
Chia-liang Kao <clkao@clkao.org>
2010-11-21 12:56:36 +0800

    allow position effect strings to be configurable

--- lib/TradeSpring/Broker/Role/Position.pm
+++ lib/TradeSpring/Broker/Role/Position.pm
@@ -8,11 +8,14 @@ requires 'on_price';
 has position => (is => "rw", isa => "Int", default => sub { 0 },
                  writer => 'set_position');
 
+has position_effect_open  => (is => "rw", isa => "Str", default => sub { 'Open' });
+has position_effect_close => (is => "rw", isa => "Str", default => sub { 'Close' });
+
 around 'submit_order' => sub {
     my $next = shift;
     my ($self, $order, %args) = @_;
     $order->{position} =
-        $self->position * $order->{dir} >= 0 ? 'Open' : 'Close';
+        $self->position * $order->{dir} >= 0 ? $self->position_effect_open : $self->position_effect_close;
 
     return $next->(@_);
 };
